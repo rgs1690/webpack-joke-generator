11384dc5473c6a4318e80cc7f95e9c7f44b0632e
Grace Sutherland <rgs1690@gmail.com>
2021-10-04 18:10:48 -0500

    setup files

---
+++ src/scripts/domBuilder.js
@@ -0,0 +1,10 @@
+const domBuilder = () => {
+  document.querySelector('#app').innerHTML = `
+    <div id="main-container">
+    <div id="view">
+    <div id="button-container">
+    </div> 
+`;
+};
+
+export default domBuilder;
---
+++ src/scripts/helpers/clearDom.js
@@ -0,0 +1,6 @@
+const clearDom = () => {
+  document.querySelector('#view').innerHTML = '';
+  document.querySelector('#button-container').innerHTML = '';
+};
+
+export default clearDom;
